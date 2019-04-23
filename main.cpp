41317f5001adb0c3808042e13b4a5e5c8315331d
k0mach1 <matchbaske.3.27@gmail.com>
2019-04-23 18:26:02 +0900

    add some class and struct

--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <random>
+
+using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+class Particle {
+private:
+    Position position;
+    Position best_position;
+    Velocity velocity;
+public:
+    Particle(float x_min, float x_max, float y_min, float y_max) {
+        std::random_device rd{};
+    }
+
+    void update_position() {
+        float new_x = position.x + velocity.x;
+        float new_y = position.y + velocity.y;
+        this->position = Position(new_x, new_y);
+    }
+
+    void update_velocity(float x, float y) {
+
+    }
+};
+
+struct Position {
+private:
+    float x;
+    float y;
+public:
+    Position(float x, float y) {
+        this->x = x;
+        this->y = y;
+    }
+};
+
+struct Velocity {
+private:
+    float x;
+    float y;
+public:
+    Velocity(float x, float y) {
+        this->x = x;
+        this->y = y;
+    }
+};
