03d28c3e6b8025b00b1876b0cc4f614304615c3a
Kosuke Machida <matchbaske.3.27@gmail.com>
2019-05-12 22:43:09 +0900

    add 10D calculaion

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <random>
 #include <tuple>
 #include <cmath>
-#include <fstream>
 
 struct Vector {
 public:
@@ -50,8 +49,106 @@ Vector operator *(const float a, const Vector u){	//内積 vector*vector
     return w;
 }
 
-// MARK ::
+struct Vector10D {
+    int n = 10;
 
+    float a;
+    float b;
+    float c;
+    float d;
+    float e;
+    float f;
+    float g;
+    float h;
+    float i;
+    float j;
+
+    Vector10D() {
+        this->a = 0.0;
+        this->b = 0.0;
+        this->c = 0.0;
+        this->d = 0.0;
+        this->e = 0.0;
+        this->f = 0.0;
+        this->g = 0.0;
+        this->h = 0.0;
+        this->i = 0.0;
+        this->j = 0.0;
+    }
+
+    Vector10D(float a, float b, float c, float d, float e, float f, float g, float h, float i, float j) {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+        this->d = d;
+        this->e = e;
+        this->f = f;
+        this->g = g;
+        this->h = h;
+        this->i = i;
+        this->j = j;
+    }
+
+    Vector10D& operator = (const Vector10D& v) {
+        this->a = v.a;
+        this->b = v.b;
+        this->c = v.c;
+        this->d = v.d;
+        this->e = v.e;
+        this->f = v.f;
+        this->g = v.g;
+        this->h = v.h;
+        this->i = v.i;
+        this->j = v.j;
+
+        return *this;
+    }
+};
+
+// MARK :: Binary Operator for Vector10D Class
+Vector10D operator +(const Vector10D u, const Vector10D v) {
+    Vector10D w;
+    w.a = u.a + v.a;
+    w.b = u.b + v.b;
+    w.c = u.c + v.c;
+    w.d = u.d + v.d;
+    w.e = u.e + v.e;
+    w.f = u.f + v.f;
+    w.g = u.g + v.g;
+    w.h = u.h + v.h;
+    w.i = u.i + v.i;
+    w.j = u.j + v.j;
+    return w;
+}
+
+Vector10D operator -(const Vector10D u, const Vector10D v) {
+    Vector10D w;
+    w.a = u.a - v.a;
+    w.b = u.b - v.b;
+    w.c = u.c - v.c;
+    w.d = u.d - v.d;
+    w.e = u.e - v.e;
+    w.f = u.f - v.f;
+    w.g = u.g - v.g;
+    w.h = u.h - v.h;
+    w.i = u.i - v.i;
+    w.j = u.j - v.j;
+    return w;
+}
+
+Vector10D operator *(const float a, const Vector10D u){	//内積 vector*vector
+    Vector10D w;
+    w.a = u.a * a;
+    w.b = u.b * a;
+    w.c = u.c * a;
+    w.d = u.d * a;
+    w.f = u.f * a;
+    w.g = u.g * a;
+    w.h = u.h * a;
+    w.i = u.i * a;
+    w.j = u.j * a;
+    return w;
+}
 class Particle {
 private:
     float w  = 0.5;
@@ -79,6 +176,7 @@ private:
             return 0.0;
         }
     }
+
 public:
     Particle() {}
 
@@ -123,6 +221,92 @@ public:
     }
 };
 
+class Particle10D {
+    float w  = 0.5;
+    float c1 = 1.0;
+    float c2 = 1.0;
+
+    Vector10D      position;
+    Vector10D best_position;
+    Vector10D      velocity;
+    float        best_value;
+    int         problem_num;
+
+    float function_value() {
+        if (problem_num == 0) {
+            return position.a * position.a + position.b * position.b + position.c * position.c
+                + position.d * position.d + position.e * position.e + position.f * position.f + position.g * position.g
+                + position.h * position.h + position.i * position.i + position.j * position.j;
+        } else if (problem_num == 1) {
+            return 10 * position.n
+                + (position.a * position.a - 10 * std::cos(2 * M_PI * position.a))
+                + (position.b * position.b - 10 * std::cos(2 * M_PI * position.b))
+                + (position.c * position.c - 10 * std::cos(2 * M_PI * position.c))
+                + (position.d * position.d - 10 * std::cos(2 * M_PI * position.d))
+                + (position.e * position.e - 10 * std::cos(2 * M_PI * position.e))
+                + (position.f * position.f - 10 * std::cos(2 * M_PI * position.f))
+                + (position.g * position.g - 10 * std::cos(2 * M_PI * position.g))
+                + (position.h * position.h - 10 * std::cos(2 * M_PI * position.h))
+                + (position.i * position.i - 10 * std::cos(2 * M_PI * position.i))
+                + (position.j * position.j - 10 * std::cos(2 * M_PI * position.j));
+        } else if (problem_num == 2) {
+            return 1
+                + (position.a * position.a + position.b * position.b + position.c * position.c
+                    + position.d * position.d + position.e * position.e + position.f * position.f + position.g * position.g
+                    + position.h * position.h + position.i * position.i + position.j * position.j) / 4000
+                - std::cos(position.a / std::sqrt(1)) * std::cos(position.b / std::sqrt(2))
+                    * std::cos(position.c / std::sqrt(3)) * std::cos(position.d / std::sqrt(4))
+                    * std::cos(position.e / std::sqrt(5)) * std::cos(position.f / std::sqrt(6))
+                    * std::cos(position.g / std::sqrt(7)) * std::cos(position.h / std::sqrt(8))
+                    * std::cos(position.i / std::sqrt(9)) * std::cos(position.j / std::sqrt(10));
+        } else {
+            return 0.0;
+        }
+    }
+
+public:
+    Particle10D() {}
+
+    Particle10D(Vector10D position, int problem_num) {
+        this->position      = position;
+        this->best_position = position;
+        this->velocity      = Vector10D();
+        this->problem_num   = problem_num;
+        this->best_value    = function_value();
+    }
+
+    Vector10D get_best_position() {
+        return best_position;
+    }
+
+    float get_best_value() {
+        return best_value;
+    }
+
+    void update_position() {
+        this->position = position + velocity;
+    }
+
+    void update_best_position_and_value() {
+        float new_value = function_value();
+        if (new_value < best_value) {
+            this->best_position = position;
+            this->best_value    = new_value;
+        }
+    }
+
+    void update_velocity(Vector10D global_best_position) {
+        // memo :: assign r1 and r2
+        std::random_device engine;
+        std::uniform_real_distribution<float> dist(0, 1.0);
+        float r1 = dist(engine);
+        float r2 = dist(engine);
+
+        this->velocity = w * velocity
+                         + (c1 * r1) * (best_position - position)
+                         + (c2 * r2) * (global_best_position - position);
+    }
+};
 
 struct PSOApplication {
 private:
@@ -138,6 +322,21 @@ private:
 
         for(int i = 0; i < particle_count; i++) {
             Particle particle = particles[i];
+            if (particle.get_best_value() < best_value) {
+                best_position = particle.get_best_position();
+                best_value    = particle.get_best_value();
+            }
+        }
+
+        return std::forward_as_tuple(best_position, best_value);
+    };
+
+    static std::tuple<Vector10D, float> calculate_best_position_and_value(Particle10D *particles, int particle_count) {
+        Vector10D best_position = particles[0].get_best_position();
+        float     best_value    = particles[0].get_best_value();
+
+        for(int i = 0; i < particle_count; i++) {
+            Particle10D particle = particles[i];
             if (particle.get_best_value() < best_value) {
                 best_position = particle.get_best_position();
                 best_value    = particle.get_best_value();
@@ -174,8 +373,6 @@ public:
 
         std::tie(best_position, best_value) = calculate_best_position_and_value(particles, particle_count);
 
-        std::ofstream outputfile("pso.txt");
-
         for(int loop = 0; loop < loop_count; loop++) {
             // memo :: update particles
             for(int i = 0; i < particle_count; i++) {
@@ -194,12 +391,61 @@ public:
 //            std::cout << "GLOBAL BEST POSITION : (" << best_position.x << ", " << best_position.y << ")" << std::endl;
 
             std::cout << best_value << std::endl;
-
-            outputfile << best_value << std::endl;
         }
         std::cout << best_position.x << ", " << best_position.y << std::endl;
+    }
 
-        outputfile.close();
+    void run10D() {
+        Particle10D particles[particle_count];
+        float       best_value;
+        Vector10D   best_position;
+
+        std::random_device engine;
+        std::uniform_real_distribution<float> x_dist(x_min, x_max);
+
+        for(int k = 0; k < particle_count; k++) {
+            float a = x_dist(engine);
+            float b = x_dist(engine);
+            float c = x_dist(engine);
+            float d = x_dist(engine);
+            float e = x_dist(engine);
+            float f = x_dist(engine);
+            float g = x_dist(engine);
+            float h = x_dist(engine);
+            float i = x_dist(engine);
+            float j = x_dist(engine);
+
+            Vector10D position = Vector10D(a, b, c, d, e, f, g, h, i, j);
+            Particle10D particle = Particle10D(position, problem_num);
+            particles[k] = particle;
+        }
+
+        std::tie(best_position, best_value) = calculate_best_position_and_value(particles, particle_count);
+
+        for(int loop = 0; loop < loop_count; loop++) {
+            // memo :: update particles
+            for(int i = 0; i < particle_count; i++) {
+                Particle10D particle = particles[i];
+                particle.update_position();
+                particle.update_velocity(best_position);
+                particle.update_best_position_and_value();
+                particles[i] = particle;
+            }
+
+            // memo :: calculate global best position and value after all particles are updated
+            std::tie(best_position, best_value) = calculate_best_position_and_value(particles, particle_count);
+
+//            std::cout << "  ----- LOOP : " << loop << " ------" << std::endl;
+//            std::cout << "GLOBAL BEST VALUE    : " << best_value << std::endl;
+//            std::cout << "GLOBAL BEST POSITION : (" << best_position.x << ", " << best_position.y << ")" << std::endl;
+
+            std::cout << best_value << std::endl;
+
+        }
+        std::cout << best_position.a << ", " << best_position.b << ", " << best_position.c
+                << ", " << best_position.d << ", " << best_position.e << ", " << best_position.f
+                << ", " << best_position.g << ", " << best_position.h << ", " << best_position.i
+                << ", " << best_position.j << std::endl;
     }
 };
 
@@ -213,15 +459,15 @@ int main() {
         std::cin >> problem_num;
         if (problem_num == 0) {
             PSOApplication application = PSOApplication(particle_count, 100, -5.0, 5.0, 0);
-            application.run();
+            application.run10D();
             break;
         } else if (problem_num == 1) {
             PSOApplication application = PSOApplication(particle_count, 100, -5.0, 5.0, 1);
-            application.run();
+            application.run10D();
             break;
         } else if (problem_num == 2) {
             PSOApplication application = PSOApplication(particle_count, 100, -600.0, 600.0, 2);
-            application.run();
+            application.run10D();
             break;
         }
     }
